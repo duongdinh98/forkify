fd7cd42498c005c1c97a006dfac5ea511d422565
FresherDuong <duongyolo@gmail.com>
2020-04-09 21:51:06 +0700

    fix broken API from Food2Fork

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,18 +1,21 @@
-import axios from 'axios';
-import {key} from '../config';
+import axios from "axios";
+import { key } from "../config";
 
 export default class Search {
-    constructor(query) {
-        this.query = query;
-    }
+  constructor(query) {
+    this.query = query;
+  }
 
-    async getResults(){
-        try {
-            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
-            this.result = res.data.recipes;
-        }
-        catch(err){
-            alert(err);
-        }
+  async getResults() {
+    try {
+      const res = await axios(
+        `http://127.0.0.1:6464/api/v1/search/${this.query}`
+      );
+      this.result = res.data.recipes;
+    } catch (err) {
+      alert(err);
     }
+  }
 }
+
+// http://127.0.0.1:6464/api/v1/search/carrot
